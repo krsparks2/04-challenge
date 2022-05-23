516834f9affb454ad184d3e0b8aba25f2cdbdac4
Keli <krsparks2@yahoo.com>
2022-05-22 18:43:05 -0700

    what I have for now

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,26 @@
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
+const setTime = document.getElementById('.timer')
+
+(function() {
+  var sec = 60;
+  function startTimer(){
+      var timer = setInterval(function(){
+          sec--;
+          document.getElementById('timer').innerHTML='00:'+sec;
+          if (sec < 0) {
+              clearInterval(timer);
+              alert("Time is up!")
+          }
+      }, 1000);
+  }
+  document.getElementById('incorrect').addEventListener('click', function() {
+      sec -= 5;
+      document.getElementById('timer').innerHTML='00:'+sec;
+  });
+  startTimer();
+})();
 
 function quiz(){
   output = [];
@@ -8,22 +28,22 @@ function quiz(){
   for (var i=0; i<questions.length; i++)
   {
     answers = [];
-    for for(letter in currentQuestion.answers) //adds html radio button for each answer to the questions
+    for (letter in currentQuestion.answers) //adds html radio button for each answer to the questions
     {
       answers.push(
-        `<label>
+        '<label>
             <input type="radio" name="question${questionNumber}" value="${letter}">
             ${letter} :
             ${currentQuestion.answers[letter]}
-          </label>`
+          </label>
       );
     }
-    output.push(//adds question and answers to page
-      `<div class="question"> ${currentQuestion.question} </div>
-      <div class="answers"> ${answers.join('')} </div>`
+    output.push(//adds question and answers to output
+      <div class="question"> ${currentQuestion.question} </div>
+      <div class="answers"> ${answers.join('')} </div>
     );
   }
-  quizContainer.innerHTML = output.join('');
+  quizContainer.innerHTML = output.join('');//prints quiz to page 
 }
 
 function showResults(){}
